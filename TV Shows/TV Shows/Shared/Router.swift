a88f11c756d9c179bec2795611b12b01a5671559
Kuglll <mark.frelih@gmail.com>
2021-07-18 22:04:17 +0200

    Implement Router

---
+++ TV Shows/TV Shows/Shared/Router.swift
@@ -0,0 +1,64 @@
+//
+//  Router.swift
+//  TV Shows
+//
+//  Created by Mark Frelih on 18.07.2021..
+//
+
+import Foundation
+import Alamofire
+
+
+let BASIC_URL = "https://tv-shows.infinum.academy/"
+
+enum Router: URLRequestConvertible{
+    
+    case register(email: String, password: String)
+    case login(email: String, password: String)
+    
+    var path: String {
+        switch self {
+        case .register:
+            return "users/"
+        case .login:
+            return "users/sign_in"
+        }
+    }
+    
+    var method: HTTPMethod{
+        switch self {
+        case .register:
+            return .post
+        case .login:
+            return .post
+        }
+    }
+    
+    var parameters: Parameters?{
+        switch self {
+        case .register(let email, let password):
+            return [
+                "email": email,
+                "password": password,
+                "password_confirmation": password
+            ]
+        case .login(let email, let password):
+            return [
+                "email": email,
+                "password": password,
+            ]
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = try URL(string: BASIC_URL.asURL()
+                                                  .appendingPathComponent(path)
+                                                  .absoluteString.removingPercentEncoding!)
+        var request = URLRequest.init(url: url!)
+        request.httpMethod = method.rawValue
+        request.timeoutInterval = TimeInterval(10*1000)
+        return try URLEncoding.default.encode(request,with: parameters)
+    }
+    
+    
+}
