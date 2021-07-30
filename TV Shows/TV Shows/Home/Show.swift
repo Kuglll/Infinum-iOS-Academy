da55a936662227c5a7c47ee2a00bdfd9750adbdc
Kuglll <mark.frelih@gmail.com>
2021-07-30 22:10:22 +0200

    Implement ShowsResponse

---
+++ TV Shows/TV Shows/Home/Show.swift
@@ -0,0 +1,22 @@
+//
+//  Show.swift
+//  TV Shows
+//
+//  Created by Mark Frelih on 30.07.2021..
+//
+
+import Foundation
+
+
+struct Show: Decodable {
+    let id: String
+    let average_rating: Int?
+    let description: String?
+    let image_url: String
+    let no_of_reviews: Int
+    let title: String
+}
+
+struct ShowsResponse: Decodable {
+    let shows: [Show]
+}
