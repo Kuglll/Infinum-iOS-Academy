b7cf1e6fd6300a68a2a6187d4b064456e40bec85
Kuglll <mark.frelih@gmail.com>
2021-07-31 12:00:32 +0200

    Add RatingView, ShowDetailsStoryboard and ShowDetailsViewController

---
+++ TV Shows/TV Shows/ShowDetails/ShowDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ShowDetailsViewController.swift
+//  TV Shows
+//
+//  Created by Mark Frelih on 31.07.2021..
+//
+
+import Foundation
+import UIKit
+
+class ShowDetailsViewController : UIViewController{
+    
+}
